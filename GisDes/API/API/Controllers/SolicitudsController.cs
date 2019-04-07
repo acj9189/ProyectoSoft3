72636e9393a0cf868e7b706f0a5517388300077f
acj9189 <acj8991@gmail.com>
2019-04-06 20:13:07 -0500

    Api Get FUNCIONAL
    Ya funciona el Get para ambas situaciones para hacer GET y COMO FALSO POST

--- GisDes/API/API/Controllers/SolicitudsController.cs
+++ GisDes/API/API/Controllers/SolicitudsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -36,40 +37,95 @@ namespace API.Controllers
             return Ok(solicitud);
         }
 
-        // PUT: api/Solicituds/5
-        [ResponseType(typeof(void))]
-        public async Task<IHttpActionResult> PutSolicitud(decimal id, Solicitud solicitud)
+        // GET: api/Solicituds/5
+        [ResponseType(typeof(Solicitud))]
+        public async Task<IHttpActionResult> GetSolicitud(string NombreIntegrante, string correo, string descripcion, string Idintegrante, string IdSemillero, string IdCoordinador)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+            string id = "";
+            SqlConnection conexionBaseDatos = new SqlConnection("Data Source=software220181.database.windows.net; " +
+                "Initial Catalog=GISDES;" +
+                "User ID=software2;" +
+                "Password=sistemas.2018;");
 
-            if (id != solicitud.Id)
-            {
-                return BadRequest();
-            }
+            SqlDataAdapter da = new SqlDataAdapter("SET IDENTITY_INSERT SOLICITUD on INSERT INTO SOLICITUD (NombreIntegrante,correo," +
+                "DESCRIPCIONPORQUEQUIEREINGRESAR,IdIntegrante,IDSEMoLLEROinvestigacion,COORDINADOR,ID)" +
+                "VALUES(" + "'" + NombreIntegrante + "'" + "," + "'" + correo + "'" + "," + "'" + descripcion + "'" + "," + Idintegrante + "," +
+                IdSemillero + "," + IdCoordinador + "," + id + ")", conexionBaseDatos);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            System.Diagnostics.Debug.WriteLine("HOLA LE MONO ES GAY>>>>>" + da.ToString());
 
-            db.Entry(solicitud).State = EntityState.Modified;
 
-            try
-            {
-                await db.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!SolicitudExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return StatusCode(HttpStatusCode.NoContent);
+            return null;
         }
+
+        // GET: api/Solicituds/5
+        [ResponseType(typeof(Solicitud))]
+        public async Task<IHttpActionResult> GetSolicitud(string dato)
+        {
+
+            string[] datos = dato.Split('<');
+
+            //System.Diagnostics.Debug.WriteLine("ENTRO .... ++++ " + dato);
+            string NombreIntegrante = datos[0];
+            string correo = datos[1];
+            string descripcion = datos[2];
+            string Idintegrante = datos[3];
+            string IdSemillero = datos[4];
+            string IdCoordinador = datos[5];
+            string id = "7";
+            SqlConnection conexionBaseDatos = new SqlConnection("Data Source=software220181.database.windows.net; " +
+                "Initial Catalog=GISDES;" +
+                "User ID=software2;" +
+                "Password=sistemas.2018;");
+   
+            SqlDataAdapter da = new SqlDataAdapter("SET IDENTITY_INSERT SOLICITUD on INSERT INTO SOLICITUD (NombreIntegrante,correo," +
+                "DESCRIPCIONPORQUEQUIEREINGRESAR,IdIntegrante,IDSEMoLLEROinvestigacion,COORDINADOR,ID)" +
+                "VALUES(" + "'" + NombreIntegrante + "'" + "," + "'" + correo + "'" + "," + "'" + descripcion + "'" + "," + Idintegrante + "," +
+                IdSemillero + "," + IdCoordinador + "," + id + ")", conexionBaseDatos);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            System.Diagnostics.Debug.WriteLine("HOLA LE MONO ES GAY>>>>>" + da.ToString());
+
+
+            return null;
+        }
+
+        //// PUT: api/Solicituds/5
+        //[ResponseType(typeof(void))]
+        //public async Task<IHttpActionResult> PutSolicitud(decimal id, Solicitud solicitud)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    if (id != solicitud.Id)
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    db.Entry(solicitud).State = EntityState.Modified;
+
+        //    try
+        //    {
+        //        await db.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!SolicitudExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+
+        //    return StatusCode(HttpStatusCode.NoContent);
+        //}
 
         //// POST: api/Solicituds
         //[ResponseType(typeof(Solicitud))]
@@ -103,70 +159,70 @@ namespace API.Controllers
         //}
 
 
-        // POST: api/Solicituds 
-        [ResponseType(typeof(Solicitud))]
-        public async Task<IHttpActionResult> PostSolicitud(string datos)
-        {
-            string[] a = datos.Split(',');
-            Solicitud solicitud = new Solicitud();
-          //  solicitud.Id = id;
-            solicitud.Correo = a[0];
-            solicitud.Coordinador =  Convert.ToDecimal(a[1]);
-            solicitud.IdIntegrante = Convert.ToDecimal(a[2]);
-            solicitud.NombreIntegrante = a[3];
-            solicitud.DescripcionPorqueQuiereIngresar = a[4];
-            solicitud.IdSemolleroInvestigacion = Convert.ToDecimal(a[5]);
+        //// POST: api/Solicituds 
+        //[ResponseType(typeof(Solicitud))]
+        //public async Task<IHttpActionResult> PostSolicitud(string datos)
+        //{
+        //    string[] a = datos.Split(',');
+        //    Solicitud solicitud = new Solicitud();
+        //  //  solicitud.Id = id;
+        //    solicitud.Correo = a[0];
+        //    solicitud.Coordinador =  Convert.ToDecimal(a[1]);
+        //    solicitud.IdIntegrante = Convert.ToDecimal(a[2]);
+        //    solicitud.NombreIntegrante = a[3];
+        //    solicitud.DescripcionPorqueQuiereIngresar = a[4];
+        //    solicitud.IdSemolleroInvestigacion = Convert.ToDecimal(a[5]);
 
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
 
-            db.Solicitud.Add(solicitud);
+        //    db.Solicitud.Add(solicitud);
 
-            try
-            {
-                await db.SaveChangesAsync();
-            }
-            catch (DbUpdateException)
-            {
-                if (SolicitudExists(solicitud.Id))
-                {
-                    return Conflict();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //    try
+        //    {
+        //        await db.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateException)
+        //    {
+        //        if (SolicitudExists(solicitud.Id))
+        //        {
+        //            return Conflict();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
 
-            return CreatedAtRoute("DefaultApi", new { id = solicitud.Id }, solicitud);
-        }
+        //    return CreatedAtRoute("DefaultApi", new { id = solicitud.Id }, solicitud);
+        //}
 
-        // DELETE: api/Solicituds/5
-        [ResponseType(typeof(Solicitud))]
-        public async Task<IHttpActionResult> DeleteSolicitud(decimal id)
-        {
-            Solicitud solicitud = await db.Solicitud.FindAsync(id);
-            if (solicitud == null)
-            {
-                return NotFound();
-            }
+        //// DELETE: api/Solicituds/5
+        //[ResponseType(typeof(Solicitud))]
+        //public async Task<IHttpActionResult> DeleteSolicitud(decimal id)
+        //{
+        //    Solicitud solicitud = await db.Solicitud.FindAsync(id);
+        //    if (solicitud == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            db.Solicitud.Remove(solicitud);
-            await db.SaveChangesAsync();
+        //    db.Solicitud.Remove(solicitud);
+        //    await db.SaveChangesAsync();
 
-            return Ok(solicitud);
-        }
+        //    return Ok(solicitud);
+        //}
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
 
         private bool SolicitudExists(decimal id)
         {
