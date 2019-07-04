674bff19a727958f1d92d5156bdeeb1a651f428f
acj9189 <acj8991@gmail.com>
2019-07-03 20:31:10 -0500

    Actu

--- GisDes/API/API/Controllers/SolicitudsController.cs
+++ GisDes/API/API/Controllers/SolicitudsController.cs
@@ -41,11 +41,17 @@ namespace API.Controllers
         [ResponseType(typeof(Solicitud))]
         public async Task<IHttpActionResult> GetSolicitud(string NombreIntegrante, string correo, string descripcion, string Idintegrante, string IdSemillero, string IdCoordinador)
         {
-            string id = "";
             SqlConnection conexionBaseDatos = new SqlConnection("Data Source=software220181.database.windows.net; " +
                 "Initial Catalog=GISDES;" +
                 "User ID=software2;" +
                 "Password=sistemas.2018;");
+            SqlDataAdapter cantidadId = new SqlDataAdapter("SELECT id FROM SOLICITUD", conexionBaseDatos);
+            DataSet toalId = new DataSet();
+            cantidadId.Fill(toalId);
+            int idSolicitud = toalId.Tables[0].Rows.Count + 1;
+
+            string id = idSolicitud.ToString();
+            
 
             SqlDataAdapter da = new SqlDataAdapter("SET IDENTITY_INSERT SOLICITUD on INSERT INTO SOLICITUD (NombreIntegrante,correo," +
                 "DESCRIPCIONPORQUEQUIEREINGRESAR,IdIntegrante,IDSEMoLLEROinvestigacion,COORDINADOR,ID)" +
@@ -62,6 +68,10 @@ namespace API.Controllers
         [ResponseType(typeof(Solicitud))]
         public async Task<IHttpActionResult> GetSolicitud(string dato)
         {
+            SqlConnection conexionBaseDatos = new SqlConnection("Data Source=software220181.database.windows.net; " +
+                "Initial Catalog=GISDES;" +
+                "User ID=software2;" +
+                "Password=sistemas.2018;");
 
             string[] datos = dato.Split('<');
 
@@ -72,11 +82,13 @@ namespace API.Controllers
             string Idintegrante = datos[3];
             string IdSemillero = datos[4];
             string IdCoordinador = datos[5];
-            string id = "7";
-            SqlConnection conexionBaseDatos = new SqlConnection("Data Source=software220181.database.windows.net; " +
-                "Initial Catalog=GISDES;" +
-                "User ID=software2;" +
-                "Password=sistemas.2018;");
+
+            SqlDataAdapter cantidadId = new SqlDataAdapter("SELECT id FROM SOLICITUD", conexionBaseDatos);
+            DataSet toalId = new DataSet();
+            cantidadId.Fill(toalId);
+            int idSolicitud = toalId.Tables[0].Rows.Count + 1;
+
+            string id = idSolicitud.ToString();
    
             SqlDataAdapter da = new SqlDataAdapter("SET IDENTITY_INSERT SOLICITUD on INSERT INTO SOLICITUD (NombreIntegrante,correo," +
                 "DESCRIPCIONPORQUEQUIEREINGRESAR,IdIntegrante,IDSEMoLLEROinvestigacion,COORDINADOR,ID)" +
@@ -91,7 +103,8 @@ namespace API.Controllers
             return null;
         }
 
-        private void hacerNotificacion(string idSolicitud, string idCoordinador, string Estado, SqlConnection conexionBaseDatos) {
+        private void hacerNotificacion(string idSolicitud, string idCoordinador, string Estado, SqlConnection conexionBaseDatos) 
+        {
 
             SqlDataAdapter cantidadId = new SqlDataAdapter("SELECT IdNotificacion FROM NOTIFICACION", conexionBaseDatos);
             DataSet toalId = new DataSet();
@@ -105,8 +118,6 @@ namespace API.Controllers
               "VALUES(" + "'" + id + "'" + ","  + "'" + idSolicitud + "'" + "," + "'" + idCoordinador + "'" + "," + "'" + Estado + "'" + ")", conexionBaseDatos);
             DataSet ds = new DataSet();
             da.Fill(ds);
-
-
         }
 
         //// PUT: api/Solicituds/5
